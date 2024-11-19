902c6cbe3d0d697112611aa2e8f55e3f1bc606a7
GitHub Action <action@github.com>
2024-11-19 04:41:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/czlan91/mybatislogformatter/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/czlan91/mybatislogformatter/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.czlan91.mybatislogformatter.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
